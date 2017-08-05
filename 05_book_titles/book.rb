0210ddcd0df06129b3b38458d24fd8bcebb865d5
Stefan Jenkins <stefan.jenkins@gmail.com>
2017-08-05 10:41:51 -0400

    Finished Ruby Excercises

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,26 @@
 class Book
-# write your code here
+
+
+  def title
+    @title
+  end
+
+  def title=(str)
+    titlewords = ["a", "an", "the", "and", "in", "of"]
+    words = str.split(" ")
+    words.map! do |word|
+      if !titlewords.include?(word)
+        word.capitalize
+      else
+        word
+      end
+
+    end
+    words[0].capitalize!
+    @title = words.join(" ")
+  end
+
+
+
+
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,16 @@
 class Timer
-  #write your code here
+
+
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    hours = @seconds / 3600
+    minutes = (@seconds - hours* 3600) / 60
+    secs = (@seconds - hours * 3600 - minutes * 60)
+
+    return "%02d:%02d:%02d" % [hours, minutes, secs]
+  end
 end
