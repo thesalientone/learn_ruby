511479cdef5ed5ee4ba0d61bba4b6449a782bc8a
Stefan Jenkins <stefan.jenkins@gmail.com>
2017-08-04 20:09:28 -0400

    Finished two more exercises

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,32 @@
-#write your code here
+def add(x, y)
+  x + y
+end
+
+def subtract(x, y)
+  x - y
+end
+
+def sum(array)
+  ans = array.reduce(:+)
+  if ans == nil
+    return 0
+  end
+  return ans
+end
+
+def multiply(*args)
+  args.reduce(:*)
+end
+
+def power(x, y)
+  x ** y
+end
+
+def factorial(x)
+  if x == 0
+    return 1
+  end
+
+  (1..x).reduce(:*)
+
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,56 @@
-#write your code here
+require "enumerator"
+
+def echo(text)
+  text
+end
+
+def shout(text)
+  text.upcase
+end
+
+def repeat(*args)
+
+  if args.length == 2
+    ans = args[0]
+    (args[1] -1).times do
+      ans += " " + args[0]
+    end
+    return ans
+  else
+    return args[0] + " " + args[0]
+  end
+
+end
+
+def start_of_word(word, num)
+  word[0...(num)]
+end
+
+def first_word(sentence)
+  words = sentence.split(" ")
+  words[0]
+
+end
+
+def titleize(words)
+
+  wordList = words.split(" ")
+  if wordList.length == 1
+    return wordList[0].capitalize
+  end
+  ans = ""
+  wordList.each_with_index do |word, index|
+
+    if index == 0
+      ans += word.capitalize
+    elsif !["over", "the", "a", "and"].include?(word)
+      ans += " " + word.capitalize
+    else
+      ans += " " + word
+
+    end
+
+  end
+
+  return ans
+end
